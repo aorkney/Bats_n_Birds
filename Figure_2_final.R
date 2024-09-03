f05ae2fdd506722afad326807512af4387c20798
aorkney <87127724+aorkney@users.noreply.github.com>
2024-09-03 07:39:35 -0400

    Update and rename Pairwise_integration_handwing_resolved.R to Figure_2_final.R

--- Figure_2_final.R
+++ Figure_2_final.R
@@ -141,7 +141,7 @@ bat.tree <- read.tree('chiroptera.no_outgroups.absolute.tre')
 pruned.tree.bats <- keep.tip(bat.tree,taxa)
 # Pruned phylogeny produced
 
-metadata <- read.csv('Bat_CT_process_list_Andrew_only.csv')
+metadata <- read.csv('Bat_CT_process_list_Andrew_only_upload_copy.csv')
 # Read bat metadata
 
 masses.bats <- metadata$Mass[ match(taxa,metadata$Shi_match) ]
